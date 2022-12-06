f8f8677e3b62a6516228cc39f31e6583dc1777b9
Ammar <100688362+AmmarShoukat@users.noreply.github.com>
2022-12-06 11:16:35 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+<script>
+jQuery(document).ready(function(){
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+   
+    
+    /* Assign empty url value to the iframe src attribute when modal hide, which stop the video playing */
+    $(".nws_model").on('hide.bs.modal', function(e){
+        $("#cartoonVideo"+$(this).attr('data-attr-model') ).attr('src', '');
+        console.log($(this).attr('data-attr-model'));
+        console.log( $("#cartoonVideo"+$(this).attr('data-attr-model') ).attr('data_backup_src') );
+      
+    });
+    /* Assign the initially stored url back to the iframe src attribute when modal is displayed again */
+    $(".nws_model").on('show.bs.modal', function(e){
+      var url = $("#cartoonVideo"+$(this).attr('data-attr-model') ).attr('data_backup_src');
+        $("#cartoonVideo"+ $(this).attr('data-attr-model') ).attr('src' , url );
+      
+    });
+});
+</script>
